3bee9c65fc3dd90927be2c8302e811188b8cdec5
hrushikesh <hrushikeshj.201cs220@nitk.edu.in>
2022-03-19 21:53:41 +0530

    Add role_assignment

--- app/models/role_assignment.rb
+++ app/models/role_assignment.rb
@@ -1,4 +1,7 @@
 class RoleAssignment < ApplicationRecord
   belongs_to :user
   belongs_to :role
+
+  validates :user_id, presence: true, uniqueness: { scope: :role_id }
+  validates :role_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  has_many :role_assignment
+  has_many :roles, through: :role_assignment
+
+  accepts_nested_attributes_for :role_assignment
+
+  validates :email, presence: true, uniqueness: true
+
+  def has_role?(role_sym)
+    roles.any? { |r| r.name.underscore.to_sym == role_sym }
+  end
 end
