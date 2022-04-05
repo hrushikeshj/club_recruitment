f2f1d67f9f20305844d4b71014a2ac0cd9c408ab
richard1615 <malavikahari02@gmail.com>
2022-04-05 18:49:43 +0530

    Initial

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,6 +7,10 @@ class ApplicationsController < ApplicationController
 
   respond_to :js, :html, :json
 
+  def submit_application
+    @clubs = Clubs.all
+  end
+
   # GET /users/:user_id/applications
   def index
     @applications = @user.application
@@ -19,7 +23,7 @@ class ApplicationsController < ApplicationController
   # GET /users/:user_id/applications/new
   def new
     ## .build_application will delete ans application if it alredy exist
-
+    
     if @user.application.nil? 
       @application = @user.build_application
     else
