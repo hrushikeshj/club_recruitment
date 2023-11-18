b5402e25c274dc54f02d1e6466cb9c4858063712
hrushikeshj <hrushi2002j@gmail.com>
2023-11-18 22:51:49 +0530

    whitelist ips

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module ClubRecruitment
 
     # _club_oauth_provider
     config.session_store :cookie_store, key: ENV.fetch("COOKIE_SESSION") { "_club_recruitment_session" }
+    if File.file?('/.dockerenv') == true
+      host_ip = `/sbin/ip route|awk '/default/ { print $3 }'`.strip
+      config.web_console.whitelisted_ips = [ host_ip ]
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
